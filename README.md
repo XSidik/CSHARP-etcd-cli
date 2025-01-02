f8e147fd74825558aa770d2edaa62487aec0f3bd
@NSidik <nursidik@pinteraktif.com>
2025-01-02 12:02:54 +0700

    create etcd CLI with C#

--- README.md
+++ README.md
@@ -1,2 +1,37 @@
 # CSHARP-etcd-cli
 A lightweight and efficient CLI tool built in C# for managing etcd, providing seamless interaction with key-value operations, cluster management, and more. 
+
+## Features
+- Connect to etcd clusters
+- Perform key-value operations (get, put, delete, list)
+- Authentication support
+
+## Installation
+1. Clone the repository:
+    ```sh
+    git clone https://github.com/XSidik/CSHARP-etcd-cli.git
+    ```
+2. Navigate to the project directory:
+    ```sh
+    cd CSHARP-etcd-cli/etcdCli
+    ```
+3. Build the project:
+    ```sh
+    dotnet build
+    ```
+
+## Usage
+1. Run the CLI tool:
+    ```sh
+    dotnet run
+    ```
+2. Use the available commands to interact with your etcd cluster.
+
+## Commands
+- `get [key]` - Retrieve the value of a key
+- `put [key] [value]` - Store a key-value pair
+- `delete [key]` - Remove a key
+- `list` - Get all list key and value
+
+## Contributing
+Contributions are welcome! Please open an issue or submit a pull request.
---
+++ etcdCli/Program.cs
@@ -0,0 +1,183 @@
+﻿using System;
+using dotnet_etcd;
+using DotNetEnv;
+using Grpc.Core;
+using Google.Protobuf;
+class EtcdCredentials
+{
+    public string? EtcdEndpoints { get; set; }
+    public string? EtcdUsername { get; set; }
+    public string? EtcdPassword { get; set; }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // for debuging
+        // Env.Load("../../../.env");
+
+        Env.Load();
+        EtcdCredentials etcdCredentials = new EtcdCredentials()
+        {
+            EtcdEndpoints = Environment.GetEnvironmentVariable("etcdEndpoints")!,
+            EtcdUsername = Environment.GetEnvironmentVariable("etcdUsername")!,
+            EtcdPassword = Environment.GetEnvironmentVariable("etcdPassword")!
+        };
+
+        var credentials = ChannelCredentials.Insecure;
+
+        EtcdClient etcdClient = new EtcdClient("http://" + etcdCredentials.EtcdEndpoints, configureChannelOptions: options =>
+        {
+            options.Credentials = credentials;
+        });
+
+        Console.WriteLine("Welcome to the etcd CLI!, (Type 'help' for commands)");
+
+
+        while (true)
+        {
+            string command = Console.ReadLine()!.ToLower().Trim();
+            string[] parts = command.Split(' ');
+
+            switch (parts[0])
+            {
+                case "help":
+                    HandleHelp();
+                    break;
+                case "set":
+                    HandleSet(etcdClient, etcdCredentials, parts[1], parts[2]);
+                    break;
+                case "get":
+                    HandleGet(etcdClient, etcdCredentials, parts[1]);
+                    break;
+                case "delete":
+                    HandleDelete(etcdClient, etcdCredentials, parts[1]);
+                    break;
+                case "list":
+                    HandleList(etcdClient, etcdCredentials);
+                    break;
+                case "exit":
+                    HandleExit();
+                    break;
+                default:
+                    Console.WriteLine("Invalid command. Type 'help' for commands.");
+                    break;
+            }
+        }
+    }
+
+    static Metadata Metadata(EtcdClient etcdClient, EtcdCredentials etcdCredentials)
+    {
+        var authRes = etcdClient.Authenticate(new Etcdserverpb.AuthenticateRequest()
+        {
+            Name = etcdCredentials.EtcdUsername,
+            Password = etcdCredentials.EtcdPassword,
+        });
+
+        var metadata = new Metadata();
+        metadata.Add(new Metadata.Entry("token", authRes.Token));
+        return metadata;
+    }
+
+    static void HandleHelp()
+    {
+        Console.WriteLine(@"Commands:
+            help                Show this help message
+            set <key> <value>   Set a key-value pair in etcd
+            get <key>           Get the value of a key from etcd
+            delete <key>        Delete a key from etcd
+            list                List all keys stored in etcd
+            exit                Exit the console
+        ");
+    }
+
+    static void HandleSet(EtcdClient etcdClient, EtcdCredentials etcdCredentials, string key, string value)
+    {
+        try
+        {
+            if (!string.IsNullOrEmpty(key) && !string.IsNullOrEmpty(value))
+            {
+                etcdClient.Put(key, value, Metadata(etcdClient, etcdCredentials));
+
+                Console.WriteLine($"Key '{key}' set with value '{value}'.");
+            }
+            else
+            {
+                Console.WriteLine("failed to set key-value pair. Please provide a key and value.");
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"failed to set key {key}: {ex.Message}");
+            throw;
+        }
+    }
+
+    static void HandleGet(EtcdClient etcdClient, EtcdCredentials etcdCredentials, string key)
+    {
+        try
+        {
+            if (!string.IsNullOrEmpty(key))
+            {
+                var res = etcdClient.Get(key, Metadata(etcdClient, etcdCredentials));
+
+                Console.WriteLine($"Key '{key}' = '{res.Kvs[0].Value.ToStringUtf8()}'.");
+            }
+            else
+            {
+                Console.WriteLine("failed to get key. Please provide a key.");
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"failed to get key {key}: {ex.Message}");
+            throw;
+        }
+    }
+
+    static void HandleDelete(EtcdClient etcdClient, EtcdCredentials etcdCredentials, string key)
+    {
+        try
+        {
+            if (!string.IsNullOrEmpty(key))
+            {
+                etcdClient.Delete(key, Metadata(etcdClient, etcdCredentials));
+
+                Console.WriteLine($"Key '{key}' deleted.");
+            }
+            else
+            {
+                Console.WriteLine("failed to delete key. Please provide a key.");
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"failed to delete key {key}: {ex.Message}");
+            throw;
+        }
+    }
+
+    static void HandleList(EtcdClient etcdClient, EtcdCredentials etcdCredentials)
+    {
+        try
+        {
+            var res = etcdClient.GetRange("", Metadata(etcdClient, etcdCredentials));
+
+            foreach (var kv in res.Kvs)
+            {
+                Console.WriteLine($"Key: '{kv.Key.ToStringUtf8()}', Value: '{kv.Value.ToStringUtf8()}'");
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"failed to list keys: {ex.Message}");
+            throw;
+        }
+    }
+
+    static void HandleExit()
+    {
+        Environment.Exit(0);
+    }
+}
